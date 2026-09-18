e13217d8774f5b3270c6d45cb52ed937c2ee7b64
Skair <skairsun@gmail.com>
2026-09-18 06:59:05 +0300

    Add try catch

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use Skair\Php\Exceptions\GroupException;
 use Skair\Php\Student;
 use Skair\Php\Group;
 use Skair\Php\Exceptions\StudentException;
@@ -23,7 +24,7 @@ $printGroupInfo = function (Group $group) : void
 
 
 
-
+try {
 $student1 = new Student("Ivan", "Ivanov", [2,3]);
 $student2 = new Student("Sergey", "Sergeev", [3, 3, 4]);
 $student3 = new Student("Kirill", "Kirillov");
@@ -32,6 +33,7 @@ $student1->addGrade(4);
 $student1->addGrade(5);
 $student2->addGrade(5);
 $student3->addGrade(2);
+
 
 
 $group = new Group("P-31", [$student1]);
@@ -47,3 +49,10 @@ $printStudentInfo($student2);
 $printStudentInfo($student3);
 echo "\n";
 $printGroupInfo($group);
+
+} catch(StudentException $e){
+    echo "StudentException: " . $e->getMessage() . "\n";
+} catch(GroupException $e){
+    echo "GroupException: " . $e->getMessage() . "\n";
+}
+
